28d12acde71e723e19a5633690d924caaff1b5fa
eshibudaoweng <1037436210@qq.com>
2018-01-15 22:01:01 +0800

    Update deal_images.py

--- deal_images.py
+++ deal_images.py
@@ -7,3 +7,20 @@ for root,dirs,files in os.walk(train_path):
     for name in files:
         print root + '/' + name
         #os.path.join
+
+        
+path = "/Users/kaikaidexiaomaike/Desktop/combine/"
+file = open('/Users/kaikaidexiaomaike/Desktop/combine/result.txt','w')
+
+for root,dirs,files in os.walk(train_path):
+    for name in files:
+        filepath = root + '/' + name
+        print(filepath)
+        for line in open(filepath):
+            file.writelines(line)
+        file.write('\n')  
+
+file.close()  
+
+        
+        
